6d9eec0f1f086e9bd55fbbe01647f31571df224c
Linyiqi <linyq29@gmail.com>
2021-06-29 14:11:51 +0800

    Update datasets.py
    Remove grey and fake images loading.

--- data/datasets.py
+++ data/datasets.py
@@ -59,36 +59,36 @@ class SYSU_ImageDataset(Dataset):
                 pass
         if self.transform is not None:
             img = self.transform(img)
-        if camid in self.RGB_cam:
-            got_fake_img = False
-            fake_img_path = self.fake_data_root + ''.join(img_path.split('/')[-3:]).split('.')[0] + '_fake_B.png'
-            if not osp.exists(fake_img_path):
-                raise IOError("{} does not exist".format(fake_img_path))
-            while not got_fake_img:
-                try:
-                    fake_img = Image.open(fake_img_path).convert('RGB')
-                    got_fake_img = True
-                except IOError:
-                    print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-                    pass
-            if self.transform is not None:
-                fake_img = self.transform(fake_img)
-            got_grey_img = False
-            grey_img_path = self.grey_data_root + '/'.join(img_path.split('/')[-3:]).split('.')[0] + '.png'
-            if not osp.exists(grey_img_path):
-                raise IOError("{} does not exist".format(grey_img_path))
-            while not got_grey_img:
-                try:
-                    grey_img = Image.open(grey_img_path).convert('RGB')
-                    got_grey_img = True
-                except IOError:
-                    print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-                    pass
-            if self.transform is not None:
-                grey_img = self.transform(grey_img)
-            return {'real': img, 'fake': fake_img, 'grey': grey_img}, pid, camid, {'real': img_path, 'fake': fake_img_path, 'grey': grey_img_path}
-        else:
-            return {'real': img}, pid, camid, {'real': img_path}
+#         if camid in self.RGB_cam:
+#             got_fake_img = False
+#             fake_img_path = self.fake_data_root + ''.join(img_path.split('/')[-3:]).split('.')[0] + '_fake_B.png'
+#             if not osp.exists(fake_img_path):
+#                 raise IOError("{} does not exist".format(fake_img_path))
+#             while not got_fake_img:
+#                 try:
+#                     fake_img = Image.open(fake_img_path).convert('RGB')
+#                     got_fake_img = True
+#                 except IOError:
+#                     print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+#                     pass
+#             if self.transform is not None:
+#                 fake_img = self.transform(fake_img)
+#             got_grey_img = False
+#             grey_img_path = self.grey_data_root + '/'.join(img_path.split('/')[-3:]).split('.')[0] + '.png'
+#             if not osp.exists(grey_img_path):
+#                 raise IOError("{} does not exist".format(grey_img_path))
+#             while not got_grey_img:
+#                 try:
+#                     grey_img = Image.open(grey_img_path).convert('RGB')
+#                     got_grey_img = True
+#                 except IOError:
+#                     print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+#                     pass
+#             if self.transform is not None:
+#                 grey_img = self.transform(grey_img)
+#             return {'real': img, 'fake': fake_img, 'grey': grey_img}, pid, camid, {'real': img_path, 'fake': fake_img_path, 'grey': grey_img_path}
+#         else:
+        return {'real': img}, pid, camid, {'real': img_path}
 
 class RegDB_ImageDataset(Dataset):
     """Image Person ReID Dataset"""
@@ -117,33 +117,33 @@ class RegDB_ImageDataset(Dataset):
                 pass
         if self.transform is not None:
             img = self.transform(img)
-        if camid in self.RGB_cam:
-            got_fake_img = False
-            fake_img_path = self.fake_data_root + ''.join(img_path.split('/')[-2:]).split('.')[0] + '_fake_B.bmp'
-            if not osp.exists(fake_img_path):
-                raise IOError("{} does not exist".format(fake_img_path))
-            while not got_fake_img:
-                try:
-                    fake_img = Image.open(fake_img_path).convert('RGB')
-                    got_fake_img = True
-                except IOError:
-                    print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-                    pass
-            if self.transform is not None:
-                fake_img = self.transform(fake_img)
-            got_grey_img = False
-            grey_img_path = self.grey_data_root + '/'.join(img_path.split('/')[-2:]).split('.')[0] + '.png'
-            if not osp.exists(grey_img_path):
-                raise IOError("{} does not exist".format(grey_img_path))
-            while not got_grey_img:
-                try:
-                    grey_img = Image.open(grey_img_path).convert('RGB')
-                    got_grey_img = True
-                except IOError:
-                    print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
-                    pass
-            if self.transform is not None:
-                grey_img = self.transform(grey_img)
-            return {'real': img, 'fake': fake_img, 'grey': grey_img}, pid, camid, {'real': img_path, 'fake': fake_img_path, 'grey': grey_img_path}
-        else:
-            return {'real': img }, pid, camid, {'real': img_path }
+#         if camid in self.RGB_cam:
+#             got_fake_img = False
+#             fake_img_path = self.fake_data_root + ''.join(img_path.split('/')[-2:]).split('.')[0] + '_fake_B.bmp'
+#             if not osp.exists(fake_img_path):
+#                 raise IOError("{} does not exist".format(fake_img_path))
+#             while not got_fake_img:
+#                 try:
+#                     fake_img = Image.open(fake_img_path).convert('RGB')
+#                     got_fake_img = True
+#                 except IOError:
+#                     print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+#                     pass
+#             if self.transform is not None:
+#                 fake_img = self.transform(fake_img)
+#             got_grey_img = False
+#             grey_img_path = self.grey_data_root + '/'.join(img_path.split('/')[-2:]).split('.')[0] + '.png'
+#             if not osp.exists(grey_img_path):
+#                 raise IOError("{} does not exist".format(grey_img_path))
+#             while not got_grey_img:
+#                 try:
+#                     grey_img = Image.open(grey_img_path).convert('RGB')
+#                     got_grey_img = True
+#                 except IOError:
+#                     print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+#                     pass
+#             if self.transform is not None:
+#                 grey_img = self.transform(grey_img)
+#             return {'real': img, 'fake': fake_img, 'grey': grey_img}, pid, camid, {'real': img_path, 'fake': fake_img_path, 'grey': grey_img_path}
+#         else:
+        return {'real': img }, pid, camid, {'real': img_path }
